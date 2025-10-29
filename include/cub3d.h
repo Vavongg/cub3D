b9cb14c51607b76e61e7792cf67e050929a0d7cd
Vavongg <alexis.itvg@gmail.com>
2025-10-29 13:14:37 +0100

    Ignore minilibx-linux

---
+++ include/cub3d.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ainthana <ainthana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 11:56:48 by ainthana          #+#    #+#             */
+/*   Updated: 2025/10/29 12:21:03 by ainthana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include "../minilibx-linux/mlx.h"
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+#endif
